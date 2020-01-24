9eafdb3e4cdb29134931cb0c3a7ebf20f4dd8994
Ayesha Mazumdar <ayeshakmaz@gmail.com>
2020-01-24 15:37:59 -0800

    feat(DragAndDrop): New component to handle dynamic, keyboard drag and drop (#1261)

---
+++ src/components/DragAndDrop/DragAndDrop.story.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+
+import DragAndDropStateless from './index';
+import Token from '../Token';
+
+const itemsWithoutGroups = [
+  { id: 1, type: 'item', text: 'Item A' },
+  { id: 2, type: 'item', text: 'Item B' },
+  { id: 3, type: 'item', text: 'Item C' },
+  { id: 4, type: 'item', text: 'Item D' },
+  { id: 5, type: 'item', text: 'Item E' },
+];
+
+// eslint-disable-next-line react/prop-types
+const renderTokenItem = ({ item, index, snapshot }) => (
+  <div>
+    <Token
+      isDraggable={ true }
+      name={ item['text'] + '- isDragging? ' + snapshot.isDragging }
+      order={ index + 1 }
+      showWell={ false }
+    />
+  </div>
+);
+
+// Helper wrapper class to store the state so the stories are usable/interactive
+class DragAndDrop extends React.Component {
+  state = {
+    items: this.props.items,
+  };
+
+  updateItemOrder = (items) => {
+    this.setState({ items });
+    this.props.onDragEnd(items);
+  };
+
+  render() {
+    const { renderItem, idForDroppableRegion, onBeforeCapture } = this.props;
+    return (
+      <DragAndDropStateless
+        idForDroppableRegion={ idForDroppableRegion }
+        items={ this.state.items }
+        onBeforeCapture={ onBeforeCapture }
+        onDragEnd={ this.updateItemOrder }
+        renderItem={ renderItem }
+      />
+    );
+  }
+}
+
+DragAndDrop.propTypes = DragAndDropStateless.propTypes;
+DragAndDrop.defaultProps = DragAndDropStateless.defaultProps;
+
+const stories = storiesOf('DragAndDrop', module);
+stories
+  .addDecorator(withKnobs)
+  .addDecorator(story => <div id="root-preview">{story()}</div>);
+
+stories.add('Reordering flat list', () => {
+  return (
+    <DragAndDrop
+      idForDroppableRegion={ 'droppable-story-demo' }
+      items={ itemsWithoutGroups }
+      onBeforeCapture={ action('do something before dragging begins') }
+      onDragEnd={ action('get new order of items') }
+      renderItem={ renderTokenItem }
+    />
+  );
+});
---
+++ src/components/DragAndDrop/tests/index.js
@@ -0,0 +1,40 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { mount } from 'enzyme';
+
+import DragAndDrop from '../index';
+import Token from '../../Token';
+
+const itemsWithoutGroups = [
+  { id: 1, type: 'item', text: 'Item A' },
+  { id: 2, type: 'item', text: 'Item B' },
+  { id: 3, type: 'item', text: 'Item C' },
+  { id: 4, type: 'item', text: 'Item D' },
+  { id: 5, type: 'item', text: 'Item E' },
+];
+
+const renderTokenItem = ({ item, index, snapshot }) => (
+  <div>
+    <Token
+      isDraggable={ true }
+      name={ item['text'] + '- isDragging? ' + snapshot.isDragging }
+      order={ index + 1 }
+      showWell={ false }
+    />
+  </div>
+);
+
+describe('components/DragAndDrop', function() {
+  it('renders multiple draggable items', () => {
+    const component = mount(
+      <DragAndDrop
+        items={ itemsWithoutGroups }
+        renderItem={ renderTokenItem }
+        idForDroppableRegion={ 'droppable-test-demo' }
+      />
+    );
+    expect(component.find('li').length).toBe(5);
+    expect(component.find('.oui-token').length).toBe(5);
+  });
+
+});
